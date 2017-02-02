0b6790c4ae9539b45d1491cc0029c1e97dc12f5a
Juan Felipe <felipemesa14@gmail.com>
2017-02-02 08:17:28 -0500

    Montaje proyecto en laravel con composer

---
+++ app/Providers/EventServiceProvider.php
@@ -0,0 +1,32 @@
+<?php
+
+namespace Prontofix\Providers;
+
+use Illuminate\Support\Facades\Event;
+use Illuminate\Foundation\Support\Providers\EventServiceProvider as ServiceProvider;
+
+class EventServiceProvider extends ServiceProvider
+{
+    /**
+     * The event listener mappings for the application.
+     *
+     * @var array
+     */
+    protected $listen = [
+        'Prontofix\Events\SomeEvent' => [
+            'Prontofix\Listeners\EventListener',
+        ],
+    ];
+
+    /**
+     * Register any events for your application.
+     *
+     * @return void
+     */
+    public function boot()
+    {
+        parent::boot();
+
+        //
+    }
+}
---
+++ resources/views/auth/login.blade.php
@@ -0,0 +1,9 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: Juan Felipe
+ * Date: 27/01/2017
+ * Time: 12:12 PM
+ */
+
+echo 'Hola Mundo';
